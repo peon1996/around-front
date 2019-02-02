a60a3e32a5dfcdd1f572aed22833aa81bca4b788
peon1996 <xiaoyanm@usc.edu>
2019-02-02 15:42:28 -0800

    form

---
+++ src/Main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Register} from "./Register";
+
+export class Main extends React.Component {
+    render() {
+        return(
+            <Register/>
+        )
+    }
+}

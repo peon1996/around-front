a2d45c67a3e89b6723a9d8beb5e7e38b72280ee2
peon1996 <xiaoyanm@usc.edu>
2019-02-02 16:13:22 -0800

    lesson1 completed

--- src/Main.js
+++ src/Main.js
@@ -4,7 +4,9 @@ import {Register} from "./Register";
 export class Main extends React.Component {
     render() {
         return(
-            <Register/>
+            <div className="main">
+                <Register/>
+            </div>
         )
     }
 }

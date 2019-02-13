d299720dd1a6cc99626fb28495aedb6e0dab3c05
peon1996 <xiaoyanm@usc.edu>
2019-02-12 19:35:21 -0800

    geolocation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import { Tabs, Button} from 'antd';
+import { GEO_OPTIONS} from "../constants";
 
 const TabPane = Tabs.TabPane;
+
 
 function callback(key) {
     console.log(key);
 }
 
 export class Home extends React.Component {
+    componentDidMount() {
+        if('geolocation' in navigator) {
+            navigator.geolocation.getCurrentPosition(this.onSuccessLoadGeoLocation, this.onFailedLoadGeoLocation,
+                GEO_OPTIONS);
+        } else {
+
+        }
+    }
+
+    onSuccessLoadGeoLocation = (position) => {
+        console.log('success', position);
+    }
+
+    onFailedLoadGeoLocation = (err) => {
+        console.log('failed', err);
+    }
+
+
     render() {
         const operations = <Button type="primary">Create New Post</Button>;
         return (

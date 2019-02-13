c66fef4e1633774b168920442040bb76d688b447
peon1996 <xiaoyanm@usc.edu>
2019-02-12 23:07:51 -0800

    spinner

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,30 +1,52 @@
 import React from 'react';
-import { Tabs, Button} from 'antd';
-import { GEO_OPTIONS} from "../constants";
+import { Tabs, Button, Spin} from 'antd';
+import { GEO_OPTIONS } from "../constants";
 
 const TabPane = Tabs.TabPane;
 
-
-function callback(key) {
-    console.log(key);
-}
-
 export class Home extends React.Component {
+    state = {
+        isLoadingGeoLocation: false,
+        error: '',
+    }
+
     componentDidMount() {
         if('geolocation' in navigator) {
+            this.setState({
+                isLoadingGeoLocation: true,
+                error: '',
+            });
             navigator.geolocation.getCurrentPosition(this.onSuccessLoadGeoLocation, this.onFailedLoadGeoLocation,
                 GEO_OPTIONS);
         } else {
-
+            this.setState({error: 'Geolocation is not supported.'});
         }
     }
 
     onSuccessLoadGeoLocation = (position) => {
+        this.setState({
+            isLoadingGeoLocation: false,
+            error: '',
+        })
         console.log('success', position);
     }
 
     onFailedLoadGeoLocation = (err) => {
-        console.log('failed', err);
+        this.setState({
+            isLoadingGeoLocation: false,
+            error: 'Failed to get geolocation' + err.message
+        });
+    }
+
+    getImagePosts = () => {
+        const { error, isLoadingGeoLocation } = this.state;
+        if(error) {
+            return <div>{error}</div>;
+        } else if (isLoadingGeoLocation) {
+            return <Spin tip="Loading geo location..."/>;
+        } else {
+            return <div>Nothing to show.</div>;
+        }
     }
 
 
@@ -33,7 +55,7 @@ export class Home extends React.Component {
         return (
             <Tabs className="main-tabs" tabBarExtraContent={operations} className="main-tabs">
                 <TabPane tab="Image Posts" key="1">
-
+                    {this.getImagePosts()}
                 </TabPane>
                 <TabPane tab="Video Posts" key="2">Content of tab 2</TabPane>
                 <TabPane tab="Map" key="3">Content of tab 3</TabPane>

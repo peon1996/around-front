06affc8f83142493ad6965131ecdaabbc187571f
peon1996 <xiaoyanm@usc.edu>
2019-02-12 19:14:46 -0800

    tab-bar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Tabs, Button} from 'antd';
+
+const TabPane = Tabs.TabPane;
+
+function callback(key) {
+    console.log(key);
+}
 
 export class Home extends React.Component {
     render() {
+        const operations = <Button type="primary">Create New Post</Button>;
         return (
-            <div>this is home</div>
+            <Tabs className="main-tabs" tabBarExtraContent={operations} className="main-tabs">
+                <TabPane tab="Image Posts" key="1">
+
+                </TabPane>
+                <TabPane tab="Video Posts" key="2">Content of tab 2</TabPane>
+                <TabPane tab="Map" key="3">Content of tab 3</TabPane>
+            </Tabs>
         );
     }
+
 }
